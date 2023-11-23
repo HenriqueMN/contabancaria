09f1ce296e30feb1e3d43b5f499ca8b5163d0159
Henrique M. N. <henriquemancininevado@gmail.com>
2023-11-23 12:31:41 -0300

    Criação do método keyPress() e tratamento de exceções para o input do usuário

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,16 +1,16 @@
 package conta;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import conta.model.Conta;
-import conta.model.ContaCorrente;
-import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		
+		/* ----- TESTES
 		// Teste da Classe Conta
 		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
 		c1.visualizar();
@@ -34,10 +34,11 @@ public class Menu {
 		cp1.visualizar();
 		cp1.depositar(5000.0f);
 		cp1.visualizar();
+		*/
 		
 		// Criação do Scanner e das variáveis
 		Scanner leia = new Scanner(System.in);
-		int opcao;
+		int opcao = 0;
 
 		while (true) {
 			
@@ -62,8 +63,16 @@ public class Menu {
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
+			
+			// Bloco de tratamento de erro para checar se o tipo de input recebido é o correto
+			try {
+				opcao = leia.nextInt();
+			}catch(InputMismatchException e){ // Exceção que indica uma entrada de dados de tipo inadequado
+				System.out.println("\nDigite valores inteiros!");
+				leia.nextLine();
+				opcao = 0; // Redefine o valor da opção para 0
+			}
 
-			opcao = leia.nextInt();
 			
 			// Verifica se a opção não é sair
 			if (opcao == 9) {
@@ -77,35 +86,35 @@ public class Menu {
 			switch (opcao) {
 				case 1: // Cria a conta
 					System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
-
+					keyPress();
 					break;
 				case 2: // Lista todas as contas
 					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
-
+					keyPress();
 					break;
 				case 3: // Consulta os dados da conta
 					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
-
+					keyPress();
 					break;
 				case 4: // Atualiza os dados da conta
 					System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
-
+					keyPress();
 					break;
 				case 5: // Apaga a conta
 					System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
-
+					keyPress();
 					break;
 				case 6: // Faz um saque
 					System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
-
+					keyPress();
 					break;
 				case 7: // Faz um depósito
 					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
-
+					keyPress();
 					break;
 				case 8: // Faz uma transferência para outra conta
 					System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
-
+					keyPress();
 					break;
 				default: // Opção inválida
 					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
@@ -121,6 +130,17 @@ public class Menu {
 		System.out.println("github.com/HenriqueMN");
 		System.out.println("*********************************************************");
 
+	}
+	
+	// Método que "pausa" o programa enquanto a tecla enter não for pressionada
+	public static void keyPress() {
+
+		try {
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+		} catch (IOException e) {
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+		}
 	}
 
 }
